c2fa627c0151f86ef0f1bda06617578984cccfaa
Sam Burney <sburney@sifnt.net.au>
2020-08-15 02:01:31 +0000

    Split out shared functions into their own module(s)

---
+++ samsung_ac/__init__.py
@@ -0,0 +1,31 @@
+import ssl
+import asyncio
+
+from . import protocol
+
+# AC connection loop
+async def connect(ac):
+    # Create SSL context
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+    ssl_context.load_cert_chain('cert/ac14k_m.pem')
+
+    # Define asyncio loop
+    loop = asyncio.get_running_loop()
+
+    # Handle closed connection
+    on_con_lost = loop.create_future()
+
+    transport = await loop.create_connection(
+        lambda: protocol.AirconditionerClientProtocol(on_con_lost, ac),
+        host=ac['ip'],
+        port=ac['port'],
+        ssl=ssl_context
+    )
+
+    # Close transpot
+    try:
+        await on_con_lost
+    finally:
+        transport.close()
